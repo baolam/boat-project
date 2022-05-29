072aa7bc6a0fc81be6ea7cf289a429f0c9f731a7
baolam <lam01662052827@gmail.com>
2022-05-29 11:25:04 +0700

    Update file's size

--- index.js
+++ index.js
@@ -38,8 +38,8 @@ ai.on("connection", (socket) => {
   });
 });
 
-app.use(express.urlencoded({ extended : false }));
-app.use(express.json());
+app.use(express.urlencoded({ extended : false, limit : "5mb" }));
+app.use(express.json({ limit : "5mb" }));
 app.get("/", (req, res) => {
   ai.sockets.emit("handle", req.body.base64);
   res.status(200).send("OK");
